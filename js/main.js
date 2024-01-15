168a955d3a6581d8aa26267bec7a640160492c36
GloriaBimbi <gloria.bimbi1@libero.it>
2024-01-15 22:50:06 +0100

    feat milesone 3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,53 @@
 // recupero gli elementi html che mi servono
-slidesContainerEl = document.getElementById("slides-container");
+const slidesContainerEl = document.getElementById("slides-container");
+const arrowBeforeEl = document.getElementById("arrow-before");
+const arrowAfterEl = document.getElementById("arrow-after");
 
 // creo l'array di immagini
-allSlides = ["01.webb", "02.webb", "03.webb", "04.webb", "05.webb"];
-console.log(allSlides);
+slides = ["01.webb", "02.webb", "03.webb", "04.webb", "05.webb"];
+console.log(slides);
+
+// creo la slide mostrata
+let slideIndex = 0;
 
 // creo un ciclo for con cui inserisco le immagini nell'html e a cui inserisco la classe active
 let slidesHtml = "";
-for (let i = 0; i < allSlides.lenght; i++) {
-  const slide = allSlides[i];
+for (let i = 0; i < slides.lenght; i++) {
+  const slide = slides[i];
 
-  let activeClass = i == 0 ? "active" : "";
+  let activeClass = i == slideIndex ? "active" : "";
 
   slidesHtml += `<img src="./img/${slide}" class="slide ${activeClass}" alt="img ${i}" />`;
 }
 
 slidesContainerEl.innerHTML = slidesHtml;
+
+// creo un evento sul click della freccia in basso (arrow next)
+arrowAfterEl.addEventListener("click", function () {
+  // elimino la classe active dalla slide mostrata
+  let oldSlide = document.querySelector(".slide.active");
+  oldSlide.classList.remove("active");
+
+  // incremento slideIndex (indice della slide mostrata)
+  slideIndex++;
+  console.log('Slide index: ' slideIndex);
+
+  // mostro la nuova slide
+  const allSlides = document.getElementsByClassName("slide");
+  const newSlide = allSlides[slideIndex];
+});
+
+// creo un evento sul click della freccia in alto (arrow before)
+arrowBeforeEl.addEventListener("click", function () {
+   // elimino la classe active dalla slide mostrata
+   let oldSlide = document.querySelector(".slide.active");
+   oldSlide.classList.remove("active");
+ 
+   // incremento slideIndex (indice della slide mostrata)
+   slideIndex--;
+   console.log('Slide index: ' slideIndex);
+ 
+   // mostro la nuova slide
+   const allSlides = document.getElementsByClassName("slide");
+   const newSlide = allSlides[slideIndex];
+});
